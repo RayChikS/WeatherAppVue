f1b0ea9339463b8169eea261510f8d7546888eec
RayChik <vatoresp@gmail.com>
2025-07-11 21:12:56 +0300

    structure-2

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,31 @@
+<script>
+export default {
+  data() {
+    return {
+      city: "",
+    };
+  },
+};
+</script>
+
 <template>
   <div class="wrapper">
     <h1 class="title">Weather Application</h1>
-    <p class="subtitle">Learn about weather in your city</p>
+    <p class="subtitle">
+      Learn about weather in {{ city == "" ? "your city." : city }}
+    </p>
     <div class="row">
       <input
         class="input"
+        v-model="city"
         type="text"
         placeholder="Enter city"
         id="inputCity"
       />
-      <button type="button" class="inputBtn">Add</button>
+      <button class="button" type="button">Get weather</button>
     </div>
   </div>
 </template>
-
-<script setup></script>
 
 <style scoped>
 .wrapper {
@@ -47,5 +58,21 @@
 }
 
 .input:focus {
+  border-bottom-color: #6e2d7d;
+}
+
+.button {
+  background: #e3bc4b;
+  color: #fff;
+  border-radius: 10px;
+  border: 2px solid #b99935;
+  padding: 10px 15px;
+  margin-left: 20px;
+  cursor: pointer;
+  transition: transform 500ms ease;
+}
+
+.button:hover {
+  transform: scale(1.1) translateY(-5px);
 }
 </style>
